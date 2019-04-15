larstvei/abstools

commit 85682dfffa5d2fe3b2bd844f9f74b657f81d0837

Add info to traces about values passed via invocations and futures

frontend/src/main/resources/erlang/absmodel/src/future.erl
@@ -37,9 +37,9 @@ start(null,_Method,_Params, _Info, _Cog, _Stack) ->
 start(Callee,Method,Params, Info, Cog, Stack) ->
     %% Create the schedule event based on the invocation event; this is because
     %% we don't have access to the caller id from the callee.
-    #event{caller_id=Cid, local_id=Lid, name=Name} =
+    #event{caller_id=Cid, local_id=Lid, info=EventInfo} =
         cog:register_invocation(Cog, Method, Params),
-    ScheduleEvent = #event{type=schedule, caller_id=Cid, local_id=Lid, name=Name},
+    ScheduleEvent = #event{type=schedule, caller_id=Cid, local_id=Lid, info=EventInfo},
     NewInfo = Info#process_info{event=ScheduleEvent},
     {ok, Ref} = gen_statem:start(?MODULE,[Callee,Method,Params,NewInfo,true,self()], []),
     wait_for_future_start(Ref, Cog, Stack),
@@ -296,18 +296,18 @@ next_state_on_okthx(Data=#data{calleetask=CalleeTask,waiting_tasks=WaitingTasks,
 
 completing({call, From}, get_references, Data=#data{value=Value}) ->
     {keep_state_and_data, {reply, From, gc:extract_references(Value)}};
-completing({call, From}, {done_waiting, Cog}, Data=#data{value=Value,event=Event}) ->
-    #event{caller_id=Cid, local_id=Lid, name=Name, reads=R, writes=W} = Event,
+completing({call, From}, {done_waiting, Cog}, Data=#data{value=Value, event=Event}) ->
+    #event{caller_id=Cid, local_id=Lid, info=Info, reads=R, writes=W} = Event,
     CompletionEvent=#event{type=await_future, caller_id=Cid,
-                           local_id=Lid, name=Name, reads=R, writes=W},
+                           local_id=Lid, info=Info, reads=R, writes=W},
     cog:register_await_future_complete(Cog, CompletionEvent),
     {keep_state_and_data, {reply, From, Value}};
-completing({call, From}, {get, Cog}, Data=#data{value=Value,event=Event}) ->
-    #event{caller_id=Cid, local_id=Lid, name=Name, reads=R, writes=W} = Event,
-    CompletionEvent=#event{type=future_read, caller_id=Cid,
-                           local_id=Lid, name=Name, reads=R, writes=W},
+completing({call, From}, {get, Cog}, Data=#data{value={ok, Value},event=Event}) ->
+    #event{caller_id=Cid, local_id=Lid, info=Info, reads=R, writes=W} = Event,
+    CompletionEvent=#event{type=future_read, caller_id=Cid, local_id=Lid,
+                           info={Info, Value}, reads=R, writes=W},
     cog:register_future_read(Cog, CompletionEvent),
-    {keep_state_and_data, {reply, From, Value}};
+    {keep_state_and_data, {reply, From, {ok, Value}}};
 completing(cast, {okthx, Task}, Data) ->
     {NextState, Data1} = next_state_on_okthx(Data, Task),
     {next_state, NextState, Data1};
@@ -322,18 +322,18 @@ completing(info, Msg, Data) ->
 
 completed({call, From}, get_references, Data=#data{value=Value}) ->
     {keep_state_and_data, {reply, From, gc:extract_references(Value)}};
-completed({call, From}, {done_waiting, Cog}, Data=#data{value=Value,event=Event}) ->
-    #event{caller_id=Cid, local_id=Lid, name=Name, reads=R, writes=W} = Event,
+completed({call, From}, {done_waiting, Cog}, Data=#data{value={ok, Value},event=Event}) ->
+    #event{caller_id=Cid, local_id=Lid, info=Info, reads=R, writes=W} = Event,
     CompletionEvent=#event{type=await_future, caller_id=Cid,
-                           local_id=Lid, name=Name, reads=R, writes=W},
+                           local_id=Lid, info=Info, reads=R, writes=W},
     cog:register_await_future_complete(Cog, CompletionEvent),
     {keep_state_and_data, {reply, From, Value}};
-completed({call, From}, {get, Cog}, Data=#data{value=Value,event=Event}) ->
-    #event{caller_id=Cid, local_id=Lid, name=Name, reads=R, writes=W} = Event,
-    CompletionEvent=#event{type=future_read, caller_id=Cid,
-                           local_id=Lid, name=Name, reads=R, writes=W},
+completed({call, From}, {get, Cog}, Data=#data{value={ok, Value},event=Event}) ->
+    #event{caller_id=Cid, local_id=Lid, info=Info, reads=R, writes=W} = Event,
+    CompletionEvent=#event{type=future_read, caller_id=Cid, local_id=Lid,
+                           info={Info, Value}, reads=R, writes=W},
     cog:register_future_read(Cog, CompletionEvent),
-    {keep_state_and_data, {reply, From, Value}};
+    {keep_state_and_data, {reply, From, {ok, Value}}};
 completed(cast, {die, _Reason}, Data) ->
     {stop, normal, Data};
 completed(cast, {okthx, _Task}, Data) ->

frontend/src/main/resources/erlang/absmodel/include/abs_types.hrl
@@ -20,7 +20,7 @@
         {type,                 % schedule | invocation | new_object | suspend | await_future | future_read | future_write
          local_id,             % A local identifier, provided by the cog
          caller_id=undefined,  % An identifier for the calling object's cog
-         name=undefined,       % The method or class name, depending on the type
+         info=undefined,       % The method or class name, depending on the type
          reads=ordsets:new(),  % A set of object fields that are read from
          writes=ordsets:new(), % A set of object fields that are written to
          time=builtin:float(ok, clock:now()) % The time of the event creation

frontend/src/main/resources/erlang/absmodel/src/cog.erl
@@ -293,15 +293,14 @@ handle_event(cast, dec_ref_count, _StateName, Data=#data{referencers=Referencers
 %% invocation.
 handle_event({call, From}, {register_invocation, Method, Params}, _StateName,
              Data=#data{next_stable_id=N, id=Id, recorded=Recorded, replaying=[]}) ->
-    Event = #event{type=invocation, caller_id=Id, local_id=N, name=[Method | Params]},
+    Event = #event{type=invocation, caller_id=Id, local_id=N, info=[Method | Params]},
     NewData = Data#data{next_stable_id=N+1, recorded=[Event | Recorded]},
     {keep_state, NewData, {reply, From, Event}};
 handle_event({call, From}, {register_invocation, Method, Params}, _StateName,
              Data=#data{next_stable_id=N, id=Id, recorded=Recorded,
                         replaying=[Event=#event{type=invocation,
                                                 caller_id=Id,
-                                                local_id=N,
-                                                name=[Method | Params]} | Rest]}) ->
+                                                local_id=N} | Rest]}) ->
     NewData = Data#data{next_stable_id=N+1, recorded=[Event | Recorded], replaying=Rest},
     {keep_state, NewData, {reply, From, Event}};
 
@@ -311,21 +310,21 @@ handle_event({call, From}, {register_new_object, Class}, _StateName,
                         recorded=[Event=#event{type=new_object,
                                                caller_id=Id,
                                                local_id=N,
-                                               name=Class} | Recorded]},
+                                               info=Class} | Recorded]},
     {keep_state, NewData, {reply, From, Event}};
 handle_event({call, From}, {register_new_object, Class}, _StateName,
              Data=#data{next_stable_id=N, id=Id, recorded=Recorded,
                         replaying=[Event=#event{type=new_object,
                                                 caller_id=Id,
                                                 local_id=N,
-                                                name=Class} | Rest]}) ->
+                                                info=Class} | Rest]}) ->
     NewData = Data#data{next_stable_id=N+1, recorded=[Event | Recorded], replaying=Rest},
     {keep_state, NewData, {reply, From, Event}};
 
 handle_event({call, From}, {register_new_local_object, Class}, _StateName,
              Data=#data{next_stable_id=N, id=Id, recorded=Recorded, replaying=[]}) ->
-    Event1 = #event{type=new_object, caller_id=Id, local_id=N, name=Class},
-    Event2 = #event{type=schedule, caller_id=Id, local_id=N, name=init},
+    Event1 = #event{type=new_object, caller_id=Id, local_id=N, info=Class},
+    Event2 = #event{type=schedule, caller_id=Id, local_id=N, info=init},
     NewRecorded = [Event2, Event1 | Recorded],
     NewData = Data#data{next_stable_id=N+1, recorded=NewRecorded},
     {keep_state, NewData, {reply, From, Event1}};
@@ -334,11 +333,11 @@ handle_event({call, From}, {register_new_local_object, Class}, _StateName,
                         replaying=[Event1=#event{type=new_object,
                                                  caller_id=Id,
                                                  local_id=N,
-                                                 name=Class},
+                                                 info=Class},
                                    Event2=#event{type=schedule,
                                                  caller_id=Id,
                                                  local_id=N,
-                                                 name=init} | Rest]}) ->
+                                                 info=init} | Rest]}) ->
     NewRecorded = [Event2, Event1 | Recorded],
     NewData = Data#data{next_stable_id=N+1, recorded=NewRecorded, replaying=Rest},
     {keep_state, NewData, {reply, From, Event1}};
@@ -557,8 +556,8 @@ no_task_schedulable({call, From}, {process_runnable, TaskRef},
              {reply, From, ok}};
         T ->       % Execute T -- might or might not be TaskRef
             #process_info{event=Event} = maps:get(T, ProcessInfos),
-            #event{caller_id=Cid, local_id=Lid, name=Name} = Event,
-            NewRecorded = [#event{type=schedule, caller_id=Cid, local_id=Lid, name=Name} | Recorded],
+            #event{caller_id=Cid, local_id=Lid, info=Info} = Event,
+            NewRecorded = [#event{type=schedule, caller_id=Cid, local_id=Lid, info=Info} | Recorded],
             NewReplaying = case Replaying of [] -> []; [X | Rest] -> Rest end,
 
             cog_monitor:cog_active(self()),
@@ -614,12 +613,12 @@ process_running({call, From}, {token, R, ProcessState, ProcessInfo, ObjectState}
 
     %% Record/replay termination or suspension
     #event{caller_id=Cid, local_id=Lid,
-           name=Name, reads=Reads, writes=Writes} = ProcessInfo#process_info.event,
+           info=Info, reads=Reads, writes=Writes} = ProcessInfo#process_info.event,
     Event = case ProcessState of
                 done -> #event{type=future_write, caller_id=Cid, local_id=Lid,
-                               name=Name, reads=Reads, writes=Writes};
+                               info=Info, reads=Reads, writes=Writes};
                 _    -> #event{type=suspend, caller_id=Cid, local_id=Lid,
-                               name=Name, reads=Reads, writes=Writes}
+                               info=Info, reads=Reads, writes=Writes}
             end,
     NewRecorded = [Event | Recorded],
     NewReplaying = case Replaying of [] -> []; [Event | Tail] -> Tail end,
@@ -644,8 +643,8 @@ process_running({call, From}, {token, R, ProcessState, ProcessInfo, ObjectState}
             %% no need for `cog_monitor:active' since we were already running
             %% something
             #process_info{event=Event2} = maps:get(T, NewProcessInfos),
-            #event{caller_id=Cid2, local_id=Lid2, name=Name2} = Event2,
-            NewRecorded2 = [#event{type=schedule, caller_id=Cid2, local_id=Lid2, name=Name2} | NewRecorded],
+            #event{caller_id=Cid2, local_id=Lid2, info=Info2} = Event2,
+            NewRecorded2 = [#event{type=schedule, caller_id=Cid2, local_id=Lid2, info=Info2} | NewRecorded],
             NewReplaying2 = case NewReplaying of [] -> []; [X | Rest] -> Rest end,
 
             send_token(token, T, object_state_from_pid(T, NewProcessInfos, NewObjectStates)),
@@ -738,8 +737,8 @@ process_running(info, {'EXIT',TaskRef,_Reason},
                     %% no need for `cog_monitor:active' since we were already
                     %% running something
                     #process_info{event=Event} = maps:get(T, NewProcessInfos),
-                    #event{caller_id=Cid, local_id=Lid, name=Name} = Event,
-                    NewRecorded = [#event{type=schedule, caller_id=Cid, local_id=Lid, name=Name} | Recorded],
+                    #event{caller_id=Cid, local_id=Lid, info=Info} = Event,
+                    NewRecorded = [#event{type=schedule, caller_id=Cid, local_id=Lid, info=Info} | Recorded],
                     NewReplaying = case Replaying of [] -> []; [X | Rest] -> Rest end,
 
                     send_token(token, T, object_state_from_pid(T, NewProcessInfos, ObjectStates)),
@@ -978,8 +977,8 @@ in_gc(cast, resume_world, Data=#data{referencers=Referencers,
                              Data#data{polling_tasks=gb_sets:difference(Pol, PollCrashedSet)}};
                         T ->                    % Execute T
                             #process_info{event=Event} = maps:get(T, ProcessInfos),
-                            #event{caller_id=Cid, local_id=Lid, name=Name} = Event,
-                            NewRecorded = [#event{type=schedule, caller_id=Cid, local_id=Lid, name=Name} | Recorded],
+                            #event{caller_id=Cid, local_id=Lid, info=Info} = Event,
+                            NewRecorded = [#event{type=schedule, caller_id=Cid, local_id=Lid, info=Info} | Recorded],
                             NewReplaying = case Replaying of [] -> []; [X | Rest] -> Rest end,
 
                             cog_monitor:cog_active(self()),

frontend/src/main/resources/erlang/absmodel/src/modelapi_v2.erl
@@ -340,14 +340,14 @@ construct_local_trace(LocalTrace) ->
     lists:map(fun (#{type := Type,
                      caller_id := CallerId,
                      local_id := LocalId,
-                     name := Name,
+                     info := Info,
                      reads := Reads,
                      writes := Writes,
                      time := Time}) ->
                       #event{type=Atomize(Type),
                              caller_id=Atomize(CallerId),
                              local_id=Atomize(LocalId),
-                             name=Atomize(Name),
+                             info=Atomize(Info),
                              reads=lists:map(Atomize, Reads),
                              writes=lists:map(Atomize, Writes),
                              time=Time}
@@ -364,7 +364,12 @@ local_trace_to_json_friendly(LocalTrace) ->
                       #{type => Event#event.type,
                         caller_id => Event#event.caller_id,
                         local_id => Event#event.local_id,
-                        name => Event#event.name,
+                        info => case Event#event.info of
+                                    [X | XS] -> #{method => X,
+                                                  args => abs_to_json(lists:droplast(XS))};
+                                    {_, FutValue} -> #{value => builtin:toString(null, (FutValue))};
+                                    X -> X
+                                end,
                         reads => Event#event.reads,
                         writes => Event#event.writes,
                         time => Event#event.time}

frontend/src/main/resources/erlang/absmodel/src/object.erl
@@ -83,7 +83,7 @@ new(Cog,Class,Args,CreatorCog,Stack)->
     %% Create event for scheduling the init block at the caller; this is
     %% because we don't have access to the caller id from the callee.
     #event{caller_id=Cid, local_id=Lid} = cog:register_new_object(CreatorCog, Class),
-    InitEvent = #event{type=schedule, caller_id=Cid, local_id=Lid, name=init},
+    InitEvent = #event{type=schedule, caller_id=Cid, local_id=Lid, info=init},
 
     cog:add_task(Cog,init_task,none,O,Args,
                  #process_info{event=InitEvent, method= <<".init"/utf8>>, this=O, destiny=null},

commit 272c4e413176edbfaffdae4378397dd46ba48a40

Fix bug introduced in 85682dfff
Awaiting futures was unintentionally partially broken.

frontend/src/main/resources/erlang/absmodel/src/future.erl
@@ -322,7 +322,7 @@ completing(info, Msg, Data) ->
 
 completed({call, From}, get_references, Data=#data{value=Value}) ->
     {keep_state_and_data, {reply, From, gc:extract_references(Value)}};
-completed({call, From}, {done_waiting, Cog}, Data=#data{value={ok, Value},event=Event}) ->
+completed({call, From}, {done_waiting, Cog}, Data=#data{value=Value,event=Event}) ->
     #event{caller_id=Cid, local_id=Lid, info=Info, reads=R, writes=W} = Event,
     CompletionEvent=#event{type=await_future, caller_id=Cid,
                            local_id=Lid, info=Info, reads=R, writes=W},